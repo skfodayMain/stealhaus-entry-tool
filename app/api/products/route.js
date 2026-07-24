c9b265b8667003c2ddc5b1a96bcf17c96b709fad
skfodayMain <skfoday@gmail.com>
2026-07-24 01:55:21 +0100

    Update route.js

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { supabaseAdmin as supabase } from "../../../lib/supabase";
+import { supabaseAdmin as supabase } from "../../../lib/supabase-admin";
 
 function slugify(name) {
   return name
